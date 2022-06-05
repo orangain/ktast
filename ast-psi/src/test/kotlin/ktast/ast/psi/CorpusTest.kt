98cbe276f406b38052e6eefc1163c502b354ba7a
orangain <orangain@gmail.com>
2022-06-05 15:12:44 +0900

    Now CorpusTest compares code strings
    failed: 112

--- ast-psi/src/test/kotlin/ktast/ast/psi/CorpusTest.kt
+++ ast-psi/src/test/kotlin/ktast/ast/psi/CorpusTest.kt
@@ -19,28 +19,13 @@ class CorpusTest(private val unit: Corpus.Unit) {
         try {
             val origExtrasConv = ConverterWithExtras()
             val origCode = StringUtilRt.convertLineSeparators(unit.read())
-            println("----ORIG CODE----\n$origCode\n------------")
+//            println("----ORIG CODE----\n$origCode\n------------")
             val origFile = Parser(origExtrasConv).parseFile(origCode)
             println("----ORIG AST----\n${Dumper.dump(origFile, origExtrasConv)}\n------------")
 
-            val newExtrasConv = ConverterWithExtras()
             val newCode = Writer.write(origFile, origExtrasConv)
-            println("----NEW CODE----\n$newCode\n-----------")
-
-            try {
-                val newFile = Parser(newExtrasConv).parseFile(newCode)
-                println("----NEW AST----\n${Dumper.dump(newFile, newExtrasConv)}\n------------")
-
-                // Compare files, but show difference by codes.
-                // We use if condition to ignore whitespace differences.
-                if (origFile != newFile) {
-                    assertEquals(origCode, newCode)
-                }
-            } catch (ex: Parser.ParseError) {
-                // When the parser failed to parse new code, compare files to understand wrong code easily.
-                assertEquals(origCode, newCode)
-                throw ex
-            }
+//            println("----NEW CODE----\n$newCode\n-----------")
+            assertEquals(origCode, newCode)
         } catch (e: Converter.Unsupported) {
             Assume.assumeNoException(e.message, e)
         } catch (e: Parser.ParseError) {
