dcff3f0c114228434177b0fb2a8422cabfc47b7d
orangain <orangain@gmail.com>
2022-06-19 16:51:16 +0900

    Use node instead of PsiElement as a key to store extras.

--- ast-psi/src/main/kotlin/ktast/ast/psi/ConverterWithExtras.kt
+++ ast-psi/src/main/kotlin/ktast/ast/psi/ConverterWithExtras.kt
@@ -18,16 +18,16 @@ open class ConverterWithExtras : Converter(), ExtrasMap {
     // potential reuse, we know the PSI objects are all around at the same time so it's good enough.
     protected val nodesToPsiIdentities = IdentityHashMap<Node, Int>()
     protected val psiIdentitiesToNodes = mutableMapOf<Int, Node>()
-    protected val extrasBefore = mutableMapOf<Int, List<Node.Extra>>()
-    protected val extrasWithin = mutableMapOf<Int, List<Node.Extra>>()
-    protected val extrasAfter = mutableMapOf<Int, List<Node.Extra>>()
+    protected val extrasBefore = IdentityHashMap<Node, List<Node.Extra>>()
+    protected val extrasWithin = IdentityHashMap<Node, List<Node.Extra>>()
+    protected val extrasAfter = IdentityHashMap<Node, List<Node.Extra>>()
 
     // This keeps track of ws nodes we've seen before so we don't duplicate them
     protected val seenExtraPsiIdentities = mutableSetOf<Int>()
 
-    override fun extrasBefore(v: Node) = nodesToPsiIdentities[v]?.let { extrasBefore[it] } ?: emptyList()
-    override fun extrasWithin(v: Node) = nodesToPsiIdentities[v]?.let { extrasWithin[it] } ?: emptyList()
-    override fun extrasAfter(v: Node) = nodesToPsiIdentities[v]?.let { extrasAfter[it] } ?: emptyList()
+    override fun extrasBefore(v: Node) = extrasBefore[v] ?: emptyList()
+    override fun extrasWithin(v: Node) = extrasWithin[v] ?: emptyList()
+    override fun extrasAfter(v: Node) = extrasAfter[v] ?: emptyList()
 
     override fun onNode(node: Node, elem: PsiElement?) {
         // We ignore whitespace and comments here to prevent recursion
@@ -41,9 +41,9 @@ open class ConverterWithExtras : Converter(), ExtrasMap {
         psiIdentitiesToNodes.put(elemId, node)
         // Since we've never done this element before, grab its extras and persist
         val (beforeElems, withinElems, afterElems) = nodeExtraElems(elem)
-        convertExtras(beforeElems).also { if (it.isNotEmpty()) extrasBefore[elemId] = it }
-        convertExtras(withinElems).also { if (it.isNotEmpty()) extrasWithin[elemId] = it }
-        convertExtras(afterElems).also { if (it.isNotEmpty()) extrasAfter[elemId] = it }
+        convertExtras(beforeElems).also { if (it.isNotEmpty()) extrasBefore[node] = it }
+        convertExtras(withinElems).also { if (it.isNotEmpty()) extrasWithin[node] = it }
+        convertExtras(afterElems).also { if (it.isNotEmpty()) extrasAfter[node] = it }
     }
 
     open fun nodeExtraElems(elem: PsiElement): Triple<List<PsiElement>, List<PsiElement>, List<PsiElement>> {
