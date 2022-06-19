ae83195579d357b5727a7109637eea62e7826359
orangain <orangain@gmail.com>
2022-06-19 14:22:03 +0900

    Bump version to 0.7.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ val kotlinVersion by extra { "1.7.0" }
 
 allprojects {
     group = "com.github.orangain.ktast"
-    version = "0.6.0"
+    version = "0.7.0"
 
     repositories {
         mavenCentral()
