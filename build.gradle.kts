d9547960ae3def476a1173d8d8aa7b2ee7c5d309
orangain <orangain@gmail.com>
2022-06-19 14:28:18 +0900

    Introduce git-version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,13 @@
+plugins {
+    id("com.palantir.git-version") version "0.15.0"
+}
+
+val gitVersion: groovy.lang.Closure<String> by extra
 val kotlinVersion by extra { "1.7.0" }
 
 allprojects {
     group = "com.github.orangain.ktast"
-    version = "0.7.0"
+    version = gitVersion()
 
     repositories {
         mavenCentral()
