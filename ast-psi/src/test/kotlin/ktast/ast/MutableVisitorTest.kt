13788eb38b88f5404971dc885cad126b649352c6
orangain <orangain@gmail.com>
2022-06-19 20:54:07 +0900

    Now MutableVisitor accepts MutableExtrasMap

---
+++ ast-psi/src/test/kotlin/ktast/ast/MutableVisitorTest.kt
@@ -0,0 +1,66 @@
+package ktast.ast
+
+import ktast.ast.psi.ConverterWithMutableExtras
+import ktast.ast.psi.Parser
+import org.junit.Test
+
+class MutableVisitorTest {
+    @Test
+    fun testIdentityVisitor() {
+        assertMutateAndWriteExact(
+            """
+                val x = 1
+                val y = 2
+            """.trimIndent(),
+            { v, _ -> v },
+            """
+                val x = 1
+                val y = 2
+            """.trimIndent(),
+        )
+    }
+
+    @Test
+    fun testMutableVisitor() {
+        assertMutateAndWriteExact(
+            """
+                val x = 1
+                val y = 2
+            """.trimIndent(),
+            { v, _ ->
+                if (v is Node.Expr.Name) {
+                    when (v.name) {
+                        "x" -> v.copy(name = "a")
+                        "y" -> v.copy(name = "b")
+                        else -> v
+                    }
+                } else {
+                    v
+                }
+            },
+            """
+                val a = 1
+                val b = 2
+            """.trimIndent(),
+        )
+    }
+}
+
+
+private fun assertMutateAndWriteExact(
+    origCode: String,
+    fn: (v: Node?, parent: Node) -> Node?,
+    expectedCode: String
+) {
+    val origExtrasConv = ConverterWithMutableExtras()
+    val origFile = Parser(origExtrasConv).parseFile(origCode)
+    println("----ORIG AST----\n${Dumper.dump(origFile, origExtrasConv)}\n------------")
+
+    val newFile = MutableVisitor.preVisit(origFile, origExtrasConv, fn)
+    val newCode = Writer.write(newFile, origExtrasConv)
+    kotlin.test.assertEquals(
+        expectedCode.trim(),
+        newCode.trim(),
+        "Parse -> Mutate -> Write for original code, not equal"
+    )
+}
