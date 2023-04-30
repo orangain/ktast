d31e24369bc21c8384a5273d3c360ff600d10d8a
orangain <orangain@gmail.com>
2023-05-01 00:18:53 +0900

    Use compiler of Kotlin 1.8.21

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 val gitVersion: groovy.lang.Closure<String> by extra
-val kotlinVersion by extra { "1.7.0" }
+val kotlinVersion by extra { "1.8.21" }
 
 allprojects {
     group = "com.github.orangain.ktast"
