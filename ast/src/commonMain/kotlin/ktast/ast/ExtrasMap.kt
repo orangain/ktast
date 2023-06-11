fdde98a17e6c5a02e4150026f370b288ddb08cb0
orangain <orangain@gmail.com>
2023-06-11 14:39:23 +0900

    Add KDocs to ExtrasMap and MutableExtrasMap

--- ast/src/commonMain/kotlin/ktast/ast/ExtrasMap.kt
+++ ast/src/commonMain/kotlin/ktast/ast/ExtrasMap.kt
@@ -1,7 +1,21 @@
 package ktast.ast
 
+/**
+ * Interface for mapping extras to nodes.
+ */
 interface ExtrasMap {
+    /**
+     * Returns extras before the given node.
+     */
     fun extrasBefore(v: Node): List<Node.Extra>
+
+    /**
+     * Returns extras within the given node.
+     */
     fun extrasWithin(v: Node): List<Node.Extra>
+
+    /**
+     * Returns extras after the given node.
+     */
     fun extrasAfter(v: Node): List<Node.Extra>
 }
--- ast/src/commonMain/kotlin/ktast/ast/MutableExtrasMap.kt
+++ ast/src/commonMain/kotlin/ktast/ast/MutableExtrasMap.kt
@@ -1,5 +1,11 @@
 package ktast.ast
 
+/**
+ * Interface for mapping extras to nodes and moving extras between nodes.
+ */
 interface MutableExtrasMap : ExtrasMap {
+    /**
+     * Moves extras from one node to another.
+     */
     fun moveExtras(fromNode: Node, toNode: Node)
 }
