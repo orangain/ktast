7b9fe0b0fb2d4496387f33809cff6630c8a9b9f2
orangain <orangain@gmail.com>
2023-06-17 16:32:02 +0900

    Delete property extensions

--- ast-psi/src/main/kotlin/ktast/ast/psi/PsiExtensions.kt
+++ ast-psi/src/main/kotlin/ktast/ast/psi/PsiExtensions.kt
@@ -11,8 +11,6 @@ import org.jetbrains.kotlin.psi.psiUtil.allChildren
 internal fun PsiElement.nonExtraChildren() =
     allChildren.filterNot { it is PsiComment || it is PsiWhiteSpace }.toList()
 
-internal val KtImportDirective.importKeyword: PsiElement
-    get() = findChildByType(this, KtTokens.IMPORT_KEYWORD) ?: error("Missing import keyword for $this")
 internal val KtImportDirective.asterisk: PsiElement?
     get() = findChildByType(this, KtTokens.MUL)
 
@@ -20,8 +18,6 @@ internal val KtTypeParameterList.leftAngle: PsiElement?
     get() = findChildByType(this, KtTokens.LT)
 internal val KtTypeParameterList.rightAngle: PsiElement?
     get() = findChildByType(this, KtTokens.GT)
-internal val KtTypeParameterListOwner.whereKeyword: PsiElement
-    get() = findChildByType(this, KtTokens.WHERE_KEYWORD) ?: error("No where keyword for $this")
 
 internal val KtTypeArgumentList.leftAngle: PsiElement?
     get() = findChildByType(this, KtTokens.LT)
@@ -36,29 +32,10 @@ internal val KtInitializerList.valueArgumentList: KtValueArgumentList
 internal val KtTypeAlias.equalsToken: PsiElement
     get() = findChildByType(this, KtTokens.EQ) ?: error("No equals token for $this")
 
-internal val KtDelegatedSuperTypeEntry.byKeyword: PsiElement
-    get() = byKeywordNode.psi
-internal val KtPropertyDelegate.byKeyword: PsiElement
-    get() = byKeywordNode.psi
-
 internal val KtPropertyAccessor.setKeyword: PsiElement
     get() = findChildByType(this, KtTokens.SET_KEYWORD) ?: error("No set keyword for $this")
 internal val KtPropertyAccessor.getKeyword: PsiElement
     get() = findChildByType(this, KtTokens.GET_KEYWORD) ?: error("No get keyword for $this")
-
-internal val KtCatchClause.catchKeyword: PsiElement
-    get() = findChildByType(this, KtTokens.CATCH_KEYWORD) ?: error("No catch keyword for $this")
-
-internal val KtWhileExpressionBase.whileKeyword: PsiElement
-    get() = findChildByType(this, KtTokens.WHILE_KEYWORD) ?: error("No while keyword for $this")
-internal val KtDoWhileExpression.doKeyword: PsiElement
-    get() = findChildByType(this, KtTokens.DO_KEYWORD) ?: error("No do keyword for $this")
-
-internal val KtLambdaExpression.lBrace: PsiElement
-    get() = leftCurlyBrace.psi
-internal val KtLambdaExpression.rBrace: PsiElement
-    get() = rightCurlyBrace?.psi
-        ?: error("No rBrace for $this") // It seems funny, but leftCurlyBrace is non-null, while rightCurlyBrace is nullable.
 
 internal val KtQualifiedExpression.operator: PsiElement
     get() = operationTokenNode.psi
