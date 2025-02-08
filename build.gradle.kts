c23ce6a4f019946c4624a10e8b7f241739c885a3
orangain <orangain@gmail.com>
2025-02-08 21:59:51 +0900

    Add and configure maven-publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    `maven-publish`
     kotlin("multiplatform") version "2.1.10" // Kotlin plugin is required for Dokka to work properly.
     id("org.jetbrains.dokka") version "1.9.20"
 }
@@ -24,5 +25,40 @@ allprojects {
 
     repositories {
         mavenCentral()
+    }
+}
+
+publishing {
+    publications {
+        register<MavenPublication>("maven") {
+            pom {
+                name.set(project.name)
+                description.set("Ktast is a simple library to manipulate Kotlin source code as a set of AST objects.")
+                url.set("https://github.com/orangain/ktast")
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://github.com/orangain/ktast/blob/main/LICENSE")
+                        distribution.set("repo")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("orangain")
+                        name.set("Kota Kato")
+                        email.set("orangain@gmail.com")
+                    }
+                }
+                scm {
+                    url.set("https://github.com/orangain/ktast")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            url = layout.buildDirectory.dir("staging-deploy").get().asFile.toURI()
+        }
     }
 }
